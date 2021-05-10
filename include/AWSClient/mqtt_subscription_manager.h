06e6b5dd54961b08b082977bd136065bb1e58668
Bora Özgen <oezgen@nantis.de>
2021-05-10 17:27:27 +0200

    Simplify logging config

--- include/AWSClient/mqtt_subscription_manager.h
+++ include/AWSClient/mqtt_subscription_manager.h
@@ -29,30 +29,10 @@
 #ifndef MQTT_SUBSCRIPTION_MANAGER_H_
 #define MQTT_SUBSCRIPTION_MANAGER_H_
 
-/**************************************************/
-/******* DO NOT CHANGE the following order ********/
-/**************************************************/
+#include "logging_config.h"
 
-/* Logging related header files are required to be included in the following order:
- * 1. Include the header file "logging_levels.h".
- * 2. Define LIBRARY_LOG_NAME and  LIBRARY_LOG_LEVEL.
- * 3. Include the header file "logging_stack.h".
- */
-
-/* Include header that defines log levels. */
-#include "logging_levels.h"
-
-/* Logging configuration for the Subscription Manager module. */
-#ifndef LIBRARY_LOG_NAME
-    #define LIBRARY_LOG_NAME     "Subscription Manager"
-#endif
-#ifndef LIBRARY_LOG_LEVEL
-    #define LIBRARY_LOG_LEVEL    LOG_DEBUG
-#endif
-
-#include "logging_stack.h"
-
-/************ End of logging configuration ****************/
+#undef TRACE_GROUP
+#define TRACE_GROUP "SubManager"
 
 /* Include MQTT library. */
 #include "core_mqtt.h"
